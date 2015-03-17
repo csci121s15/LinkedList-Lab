5866d69961e3c810207624896f54f7967c42b391
Josh Russett <stjkr02@moravian.edu>
2015-03-17 14:42:44 -0400

    Finished Question 7.

--- src/SortedLinkedListTest.java
+++ src/SortedLinkedListTest.java
@@ -127,4 +127,48 @@ public class SortedLinkedListTest extends TestCase {
     //Length == 3
     assertEquals(3, l.length());
   }
+  
+  public void testFinaltest() {
+    SortedLinkedList l = new SortedLinkedList();
+    
+    l.insert("Dogs", 10);
+    l.insert("Lucky Charms", 777);
+    l.insert("Cats", 5);
+    l.insert("Spaghetti", 58);
+    l.insert("Pineapples", 50);
+    l.insert("Fred", 1);
+    
+    //Index 0, the Lucky Charms
+    assertEquals("Lucky Charms", l.getName(0));
+    assertEquals(777, l.getQuantity(0));
+    assertTrue(l.isMember("Lucky Charms"));
+    
+    //Index 1, the Spaghetti
+    assertEquals("Spaghetti", l.getName(1));
+    assertEquals(58, l.getQuantity(1));
+    assertTrue(l.isMember("Spaghetti"));
+    
+    //Index 2, the Pineapples
+    assertEquals("Pineapples", l.getName(2));
+    assertEquals(50, l.getQuantity(2));
+    assertTrue(l.isMember("Pineapples"));
+    
+    //Index 3, the Dogs
+    assertEquals("Dogs", l.getName(3));
+    assertEquals(10, l.getQuantity(3));
+    assertTrue(l.isMember("Dogs"));
+    
+    //Index 4, the Cats
+    assertEquals("Cats", l.getName(4));
+    assertEquals(5, l.getQuantity(4));
+    assertTrue(l.isMember("Cats"));
+    
+    //Index 5, Fred
+    assertEquals("Fred", l.getName(5));
+    assertEquals(1, l.getQuantity(5));
+    assertTrue(l.isMember("Fred"));
+    
+    //Length == 6
+    assertEquals(6, l.length());
+  }
 }
